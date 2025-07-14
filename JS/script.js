75c870f35479aef56785eee3a305e70a74f815e5
JSAssignment2 <200618875@student.georgian.on.ca>
2025-07-14 01:23:03 -0400

    Form completed and started creating class in Javascript

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+class Smoothie{
+    //Variables for smoothie
+    size;
+    flavor;
+    addition;
+    realFruit;
+    constructor(size, flavor, addition, realFruit) {
+        this.size = size;
+        this.flavor = flavor;
+        this.addition = addition;
+        this.realFruit = realFruit;
+    }
+}
